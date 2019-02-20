7451902c2c118b8fff5f5eedafee65bf99f339f1
j-berg <j-berg@users.noreply.github.com>
2019-02-19 18:21:51 -0700

    Removed redundant sort and output final file as sam, not bam

--- xpresspipe/align.py
+++ xpresspipe/align.py
@@ -49,11 +49,10 @@ def se_align(args):
     os.system('STAR --runMode genomeGenerate --genomeDir ' + str(args_dict['intermediate_references']) + str(file[:-6]) + ' --genomeFastaFiles ' + str(args_dict['intermediate_references']) + 'genome.fa --sjdbOverhang 100 --runThreadN ' + str(args_dict['threads']) + ' --sjdbFileChrStartEnd ' + str(args_dict['alignments']) + str(file[:-6]) = '_SJ.out.tab')
 
     #STAR second pass
-    os.system('STAR --genomeDir ' + str(args_dict['intermediate_references']) + str(file[:-6]) + ' --readFilesIn ' + str(args_dict['alignments']) + str(file[:-6]) = '_SJ.out.tab --runThreadN ' + str(args_dict['threads']) + ' --outFilterMultimapScoreRange 1 --outFilterMultimapNmax 20 --outFilterMismatchNmax 10 --alignIntronMax 500000 --alignMatesGapMax 1000000 --sjdbScore 2 --alignSJDBoverhangMin 1 --genomeLoad NoSharedMemory --limitBAMsortRAM 0 --readFilesCommand cat --outFilterMatchNminOverLread 0.33 --outFilterScoreMinOverLread 0.33 --sjdbOverhang 100 --outSAMstrandField intronMotif --outSAMattributes NH HI NM MD AS XS --outSAMunmapped Within --outSAMtype BAM SortedByCoordinate --outSAMheaderHD @HD VN:1.4 --outFileNamePrefix ' + str(args_dict['alignments']) + str(file[:-6]) + '_final_')
+    os.system('STAR --genomeDir ' + str(args_dict['intermediate_references']) + str(file[:-6]) + ' --readFilesIn ' + str(args_dict['input']) + str(file) ' --runThreadN ' + str(args_dict['threads']) + ' --outFilterMultimapScoreRange 1 --outFilterMultimapNmax 20 --outFilterMismatchNmax 10 --alignIntronMax 500000 --alignMatesGapMax 1000000 --sjdbScore 2 --alignSJDBoverhangMin 1 --genomeLoad NoSharedMemory --limitBAMsortRAM 0 --readFilesCommand cat --outFilterMatchNminOverLread 0.33 --outFilterScoreMinOverLread 0.33 --sjdbOverhang 100 --outSAMstrandField intronMotif --outSAMattributes NH HI NM MD AS XS --outSAMunmapped Within --outSAMtype SAM SortedByCoordinate --outSAMheaderHD @HD VN:1.4 --outFileNamePrefix ' + str(args_dict['alignments']) + str(file[:-6]) + '_final_')
 
-    #Sort output final output file
-    os.system('samtools sort ' + str(args_dict['alignments']) + str(file[:-6]) + '_final_ -o ' + str(args_dict['alignments']) + str(file[:-6]) + '_sort.sam')
-    os.system('samtools view -q 255 ' + str(args_dict['alignments']) + str(file[:-6]) + '_sort.sam > ' + str(args_dict['alignments']) + str(file[:-6]) + '_final.sam')
+    #Create sam file with only unique hits
+    os.system('samtools view -q 255 ' + str(args_dict['alignments']) + str(file[:-6]) + '_final_Aligned.sortedByCoord.out.sam > ' + str(args_dict['alignments']) + str(file[:-6]) + '_final.sam')
 
 """
 DESCRIPTION:
