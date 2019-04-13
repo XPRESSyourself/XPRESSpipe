3feca4c351360968c22f06caf2f434ca13c6350d
j-berg <j-berg@users.noreply.github.com>
2019-04-13 14:19:39 -0600

    Building functions for bam processing

--- xpresspipe/processBAM.py
+++ xpresspipe/processBAM.py
@@ -23,29 +23,52 @@ this program.  If not, see <https://www.gnu.org/licenses/>.
 import os
 import sys
 import pandas as pd
+import numpy as np
+import math
+
+"""EXTERNAL DEPENDENCIES"""
+# samtools
 
 """Read in indexed BAM file to Pandas dataframe"""
-def read_BAM(
-    bam_file, # Must be indexed before reading in
-    threads=1):
+def read_bam(file, threads = 1):
 
     # Read in BAM file
     os.system('samtools view'
           + ' --threads ' + str(threads)
-          + ' ' + str(bam_file)
-          + ' -o ' + str(bam_file)[:-4] + '.tmp')
+          + ' ' + str(file)
+          + ' -o ' + str(file)[:-4] + '.tmp')
 
     #read sorted, unique only sam files, get most abundant length and make new file
-    sam = pd.read_csv(
-            str(bam_file)[:-4] + '.tmp',
+    bam = pd.read_csv(
+            str(file)[:-4] + '.tmp',
             sep = '\t',
             header = None,
             usecols = list(range(0, 16)),
             low_memory = False)
 
-    sam[2] = sam[2].astype(str)
+    bam[2] = bam[2].astype(str)
 
-    os.system('rm '
-              + str(bam_file)[:-4] + '.tmp')
+    os.system('rm'
+              ' ' + str(file)[:-4] + '.tmp')
 
-    return sam
+    return bam
+
+""""""
+def bam_sample(bam, number):
+
+    if bam.shape[0] < int(number):
+        raise Exception('Not enough alignments to sample')
+
+    return bam.sample(n = int(number))
+
+""""""
+def mid_coordinates(bam):
+
+    bam[16] = bam[3] + (bam[9].str.len() / 2).apply(np.floor).astype('int64')
+    mid_coordinates = bam[[2,16]]
+    return mid_coordinates.values.tolist()
+
+"""P site coordinate for 28-30mers"""
+def phased_coordinates(bam):
+
+    return phased_coordinates.values.tolist()
