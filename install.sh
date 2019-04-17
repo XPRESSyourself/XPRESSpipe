cdfefed4ed15c21f68f0bb80405ce3e004f7b64c
j-berg <j-berg@users.noreply.github.com>
2019-04-17 16:50:07 -0600

    Update

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ conda install -f XPRESSpipe/requirements.yml
 
 cd XPRESSpipe; python setup.py install; cd ../
 
-pip uninstall xpresstools
+pip uninstall -y xpresstools
 
 Rscript -e 'source("http://bioconductor.org/biocLite.R")' -e 'biocLite("Rsubread")'
 Rscript -e 'source("http://bioconductor.org/biocLite.R")' -e 'biocLite("dupRadar")'
