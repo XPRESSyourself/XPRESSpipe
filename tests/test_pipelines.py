25a04cc7e104e9e4673a05f4f7890fc28a2db2ce
j-berg <j-berg@users.noreply.github.com>
2019-05-20 18:35:21 -0600

    Update path

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-__path__, xpresspipe_arguments  =  os.path.split(__file__)
+__path__  =  os.path.dirname(os.path.realpath(__file__))
 #__path__ = '/Users/jordan/scripts/XPRESSyourself/XPRESSpipe/tests'
 __path__ = __path__ + '/'
 
