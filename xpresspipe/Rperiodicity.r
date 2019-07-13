67c3510b801efd7706d88be91bbfeb976b7b4288
j-berg <j-berg@users.noreply.github.com>
2019-07-13 01:01:49 -0600

    Put license info in function block quote style effect

--- xpresspipe/Rperiodicity.r
+++ xpresspipe/Rperiodicity.r
@@ -1,25 +1,26 @@
 #!/usr/bin/env Rscript
+license <- function() {
+  "
+  XPRESSpipe
+  An alignment and analysis pipeline for RNAseq data
+  alias: xpresspipe
 
-"""
-XPRESSpipe
-An alignment and analysis pipeline for RNAseq data
-alias: xpresspipe
+  Copyright (C) 2019  Jordan A. Berg
+  jordan <dot> berg <at> biochem <dot> utah <dot> edu
 
-Copyright (C) 2019  Jordan A. Berg
-jordan <dot> berg <at> biochem <dot> utah <dot> edu
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
 
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+  PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
+  You should have received a copy of the GNU General Public License along with
+  this program.  If not, see <https://www.gnu.org/licenses/>.
+  "
+  }
 
 # Measure library complexity of RNA-seq sample
 
