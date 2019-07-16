0b3509a0e89c6a5fb4fe7d4c1bc8d23ab3f242bf
j-berg <j-berg@users.noreply.github.com>
2019-07-16 03:32:07 -0600

    Prep for version

--- xpresspipe/__init__.py
+++ xpresspipe/__init__.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License along with
 this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.1.4-beta'
+__version__ = '0.2.0-beta'
 
 from .__main__ import *
 from .align import *
