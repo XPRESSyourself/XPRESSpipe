92e292bbf382f1f6e6b09bf12ac7be554decf987
j-berg <j-berg@users.noreply.github.com>
2019-10-22 18:47:00 -0600

    Forgot to change dev tag for project

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     },
 
     classifiers = [
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics'
         ]
